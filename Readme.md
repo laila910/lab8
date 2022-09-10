8888bc7c3882db4b4d513edd16e8cabb794c45e4
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2022-09-10 22:41:08 +0200

    solve validationform

--- Readme.md
+++ Readme.md
@@ -1,9 +1,31 @@
+# First Part
 ## Ajax Lab
 > Display users data (image, email) in html page, 
  you should fetch the data from this 
  url [link](https://reqres.in/api/users?page=2)
  the data should be fetched on page load 
+## Solve 1 in ![image](Solve/fetchData.jpg)
 
-## desc
+# Second Part 
+as in image ![image2](imgTasks/Screen%20Shot%202022-05-19%20at%2012.32.07%20PM.png) 
+
+## Solve 2 
 > Modify validation on your form and add regex to validation
+> 1. if email and password empty 
+  ![image](Solve/emptyemail%26pass.jpg)
+
+  2. if empty password
+  ![image](Solve/emptypass.jpg)
+  
+  3. if empty email
+  ![image](Solve/emptyemail.jpg)
+
+  4. if password doesn't match regular expression
+  ![image](Solve/passnotmatch.jpg)
+
+  5. if form submitted
+  ![image](Solve/formsubmitted.jpg)
+
+
+
 
--- fun.js
+++ fun.js
@@ -35,5 +35,50 @@
     }).catch(function(error) {
       console.log(error);
     });
+    
 
  
+    function login(e){
+        e.preventDefault();
+        var errors=[];
+        if(document.readyState){
+          console.log('hello');
+         
+        myForm=document.getElementsByTagName('form');
+        var Email=document.getElementsByTagName('input')[0];
+        var Pass=document.getElementsByTagName('input')[1];
+              
+        if((Email.value.length== 0) && ( Pass.value.length== 0)){
+            errors.push('email and password are empty, please enter your data');
+        }
+
+        if((Email.value.length== 0)){
+            errors.push('email is empty, please enter your data');
+        }
+
+        if(( Pass.value.length== 0)){
+            errors.push('password are empty, please enter your data');
+        }
+     ptrn=/^([a-zA-Z0-9]{8,})$/;
+   if(! ( ptrn.test(Pass.value))  ){
+    errors.push('Password must be Minimum eight characters, at least one letter and one number:');
+
+   }
+    }
+    if(errors.length > 0){
+        var ErrorsSection=document.getElementById('errors');
+        ErrorsSection.innerText=errors;
+        document.body.appendChild(ErrorsSection);
+    }else{
+        form=document.getElementById('myForm');
+      
+        console.log('submmited');
+        alert('Form Successfully Submitted');
+        window.location.reload();
+        return true;
+    }
+           
+          
+
+       }
+          
